305ddbdf95d9446d677146a3b83d8fec6b8e5056
Eirik Smedsrud <eirikds@gmail.com>
2013-09-24 17:11:36 +0200

    Updated functions - hero - topmenu

--- source/javascripts/utils.js
+++ source/javascripts/utils.js
@@ -1,8 +1,54 @@
 $(document).ready(function() {
+
+// Mobile checker
+// var isMobile = {
+//     Android: function() {
+//         return navigator.userAgent.match(/Android/i);
+//     },
+//     BlackBerry: function() {
+//         return navigator.userAgent.match(/BlackBerry/i);
+//     },
+//     iOS: function() {
+//         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+//     },
+//     Opera: function() {
+//         return navigator.userAgent.match(/Opera Mini/i);
+//     },
+//     Windows: function() {
+//         return navigator.userAgent.match(/IEMobile/i);
+//     },
+//     any: function() {
+//         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+//     }
+// };
+
+// if( isMobile.any() ) {
+//   $('.mobile-site-chooser').css('display', 'block');
+// };
+
+
+// Scroll To Navigation  
 $('.internal a').on('click', function(event){     
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top-100}, 500);
 });
+
+
+// Div reordering
+// $(window).bind("resize", listenWidth);
+//     function listenWidth( e ) {
+//         if($(window).width()<600)
+//         {
+//             console.log("Reflowiubng");
+//             $(".function-text.shifter").remove().insertAfter($(".function-image"));
+//         } else {
+//             // $("#topLinks").remove().insertBefore($("#content"));
+//         }
+//     }
+
+
+$(".test.one").insertAfter(".test.three");
+
 
 // Sticky top menu
 	var stickyNavTop = $('.top-menu-container').offset().top;  
@@ -12,9 +58,8 @@ $('.internal a').on('click', function(event){
 		var scrollTop = $(window).scrollTop();
     var windowWidth = $(window).width();
 
-    // Menuland
-		if (scrollTop > (stickyNavTop+900) && stickySet == 0 && windowWidth > 780) {
-        // $('.hero-container').css('margin-top', '80px');    
+    // Menu Sticky settings
+		if (scrollTop > (stickyNavTop+150) && stickySet == 0 && windowWidth > 780) {
 	    	$('.top-menu-container').addClass('sticky');
         $('.top-menu-container').css('margin-top', '-80px');
         $('.hero-container').addClass('sticky-padding');
@@ -28,19 +73,15 @@ $('.internal a').on('click', function(event){
 			} else if (scrollTop <= (stickyNavTop)) {
 	    	$('.top-menu-container').removeClass('sticky');
         $('.hero-container').removeClass('sticky-padding');
-
-        // $('.hero-container').css('margin-top', '0px'); 
         stickySet = 0;  
 		  }
-
-    // Scrolling elements
-    // var heroTop = (scrollTop/5);
+    // Scrolling elements Paralax
+    // var heroTop = (scrollTop/2);
+    // var menuTop = (scrollTop/6);
     // var heroOpa = (1 - (scrollTop/200));
-   
-    // $('.hero-element').css('top', heroTop+"px");
+    // $('.top-menu-container').css('top', -menuTop+"px");
+    // $('.hero-text').css('top', heroTop+"px");
     // $('.hero-element').css('opacity', heroOpa);
-
-
 	};  
 	  
 	stickyNav();  
@@ -58,55 +99,4 @@ $('.internal a').on('click', function(event){
        
       });
     });  
-
-
-
-// FORM CHANGER
-// $('.price-input').each(function() {
-//    var elem = $(this);
-
-//    // Save current value of element
-//    elem.data('oldVal', elem.val());
-
-//    // Look for changes in the value
-//    elem.bind("propertychange keyup input paste", function(event){
-//       // If value has changed...
-//       if (elem.data('oldVal') != elem.val()) {
-//        // Updated stored value
-//        elem.data('oldVal', elem.val());
-
-//        // Do action
-//        console.log("CHANGED");
-//        var totalPrice = elem.val();
-//        totalPrice = totalPrice+20;
-//        $('.price-output').val(totalPrice);
-//      }
-//    });
-//  });
-
-  // $('.event-chooser').click(function(){
-  //   event.preventDefault();
-  //   $(this).toggleClass('active-menu');
-  //   $('.event-menu').slideToggle(100);
-  // });
-
-  // $('.user-chooser').click(function(){
-  //   event.preventDefault();
-  //   $(this).toggleClass('active-menu');
-  //   $('.user-menu').slideToggle(100);
-  // });
-
-  // $('.drop-menu').hover(function(){
-  //   event.preventDefault();
-  //   $(this).toggleClass('active-menu');
-  //   $(this).next('.dropdown-menu').slideDown(100);
-  // }, function() {
-    
-  // });
-
-  // $('.dropdown-menu').hover(function (){
-  //   $(this).css('display', 'block');
-  // });
-
-
 });
